25b8c539e6130d4f5a5990e48efcb09d3497b6e2
Michael Morrison <michaelcmorrison94@gmail.com>
2020-10-08 11:59:53 -0400

    Mostly ready for submission

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance;
+    public AudioClip chirp;
+    public AudioClip squeal;
+    public AudioClip gulp;
+    public AudioClip flap;
+    public AudioClip hiss;
+    public AudioClip rattle;
+    
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public float timer;
+    private TMP_Text _text;
+
+    private void Awake()
+    {
+        _text = GetComponent<TMP_Text>();
+    }
+
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+
+        string minutes = Mathf.Floor(timer / 60).ToString("00");
+        string seconds = Mathf.Floor(timer % 60).ToString("00");
+
+        _text.text = string.Format("{0}:{1}", minutes, seconds);
+        
+        if (timer < 0.0f)
+        {
+            GameManager.Instance.TriggerEndGame();
+        }
+    }
+    
+    
+}
